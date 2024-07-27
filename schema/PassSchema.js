a0ffdefb94eba78a669b96ae182630c15c52aba3
Aloksh42 <alokbits02@gmail.com>
2024-07-27 22:28:52 +0530

    Created PassSchema

---
+++ schema/PassSchema.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const PassSchema = new mongoose.Schema(
+    {
+        websiteLink: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        username: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        uniqueName: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+    },
+    { timestamps: true }
+);
+
+export default Password = mongoose.model("Password", PassSchema);
